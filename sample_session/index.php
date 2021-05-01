c90209f68ebed15193226999dcfbcdbd03d266ab
rui-watanabe <wataru610806@gmail.com>
2021-05-01 13:55:26 +0900

    connect DB with PDO

---
+++ sample_session/index.php
@@ -0,0 +1,29 @@
+<?php
+  session_start();
+  $_SESSION['session_message'] = 'session_data';
+?>
+<!doctype html>
+<html lang="ja">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="../css/style.css">
+
+<title>PHP</title>
+</head>
+<body>
+<header>
+<h1 class="font-weight-normal">PHP</h1>    
+</header>
+
+<main>
+<h2>Practice</h2>
+<pre>
+  <a href="page02.php">to page02</a>
+</pre>
+</main>
+</body>    
+</html>
